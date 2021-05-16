7cda485caf33db8830163360cf82d46a2233edad
Alexander Echeverry Torres <alexanderecheverry11@gmail.com>
2021-05-15 20:21:49 -0500

    Creación de migraciones para las 2 tablas(producto, marca)

---
+++ database/migrations/2021_05_16_010719_create_producto_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('producto', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer("id_marca");
+            $table->string("nombre");
+            $table->string("talla");
+            $table->text("observaciones");
+            $table->integer("cantidad_inventario");
+            $table->date("fecha_embarque");
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('producto');
+    }
+}
