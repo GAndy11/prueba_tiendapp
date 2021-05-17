34ca3b06911d83ce2269e5b38a42173fd9ec078d
Alexander Echeverry Torres <alexanderecheverry11@gmail.com>
2021-05-17 12:29:46 -0500

    Creación del menu en el layout principal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,16 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Tiendapp') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+   
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -25,7 +31,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand app-name" href="{{ url('/home') }}">
-                    {{ config('app.name', 'MercaTodo') }}
+                    {{ config('app.name', 'Tiendapp') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -39,49 +45,16 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarme') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            @can('admin')
-                                <li>
-                                    <div class="btn-group">
-                                        <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Administración
-                                        </button>
-                                        <div class="dropdown-menu">
-                                            <a class="dropdown-item" href=" {{ url('/users') }} ">Usuarios</a>
-                                            <a class="dropdown-item" href=" {{ url('/products') }} ">Productos</a>
-                                        </div>
-                                    </div>
-                                </li>
-                            @endcan
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Cerrar Sesión') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                            
-                        @endguest
+                        <li>
+                            <div class="btn-group">
+                                <button type="button" class="btn" aria-haspopup="true" aria-expanded="false">
+                                    <a class="dropdown-item" href=" {{ url('/marca') }} ">Marcas</a>
+                                </button>
+                                <button type="button" class="btn" aria-haspopup="true" aria-expanded="false">
+                                <a class="dropdown-item" href=" {{ url('/producto') }} ">Productos</a>
+                                </button>
+                            </div>
+                        </li>
                     </ul>
                 </div>
             </div>
